64b158252b6981e9ddedac887bc5905b963c3488
Abhijit Pusti <abhijitpusti494@gmail.com>
2025-05-14 01:56:08 +0530

    Feat: Added mongoose dbconfig

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ const bodyParser = require('body-parser');
 const { PORT } = require('./config/sever.config');
 const apiRouter = require('./routes');
 const errorHandler = require('./utils/ErrorHandler');
+const connectToDB = require('./config/db.config');
 
 const app = express();
 
@@ -22,8 +23,11 @@ app.get('/ping', (req,res) => {
 //last middleware if any error comes
 app.use(errorHandler)
 
-app.listen(PORT, () =>{
+app.listen(PORT, async () =>{
     console.log(`Server started at PORT: ${PORT}`);
+    await connectToDB();
+    console.log("Successfull connected to DB");
+    //
     // try {
     //     throw new BaseError("Some error", 401 ,"Resource not found", {});
     // } catch (error) {
